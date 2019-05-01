f9c8cf2c6c28bc5fbb4129c1c7574ec201fce516
patpalmerston <patpalmerston@gmail.com>
2019-05-01 10:00:12 -0700

    custum middleware

--- server.js
+++ server.js
@@ -9,14 +9,28 @@ const server = express();
 server.use(express.json());
 server.use(helmet());// protects projects always use
 server.use(morgan('dev'))// info on console
+server.use(teamNamer);
+
+// costum middleware
+
+// server.use((req, res, next) => {
+//   res.status(404).send('no time for it')
+// })
+
+
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
     `);
 });
+
+function teamNamer(req, res, next) {
+  req.team = 'Lambda Students';
+  next();
+}
 
 module.exports = server;
