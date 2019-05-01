91afb1f664323082b0d924588d8af87d026ce971
patpalmerston <patpalmerston@gmail.com>
2019-05-01 11:23:01 -0700

    name only added

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ server.use(express.json());
 server.use(helmet());// protects projects always use
 server.use(morgan('dev'))// info on console
 server.use(teamNamer);
+// server.use(only); - if you did this it would apply it to every end point. the point of this middleware is for only one endpoint.
+
 // server.use(moodyGateKeeper);
 
 
@@ -21,7 +23,7 @@ server.use(teamNamer);
 
 
 
-server.use('/api/hubs',restricted, hubsRouter); // stick in middleware (restricted), daisy chain affect to intercept the process of calling the router.
+server.use('/api/hubs',restricted, only('frodo'), hubsRouter); // stick in middleware (restricted), daisy chain affect to intercept the process of calling the router.
 
 server.get('/', (req, res, next) => {
   res.send(`
@@ -36,7 +38,7 @@ function teamNamer(req, res, next) {
   req.team = 'Lambda Students';
   next();
 }
-
+//can toggle on or off
 function moodyGateKeeper(req, res, next) {
   const seconds = new Date().getSeconds();
   if (seconds % 3 === 0) {
@@ -52,6 +54,19 @@ function restricted(req, res, next) {
     next();
   } else {
     res.status(401).json({ message: 'Invalid credentials'})
+  }
+}
+
+/// this middleware is not working postman wont load
+function only(name){
+  //returns the middleware
+    return function(req, res, next) {
+    const personName = req.headers.name || '';// just in case there is no header provided
+    if(personName.toLowerCase() === name.toLowerCase()){
+      next();
+    } else {
+      res.status(401).json({ message: 'you have no access to this resource'})
+    }
   }
 }
 
